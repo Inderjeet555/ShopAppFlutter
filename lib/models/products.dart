d56c4ad848b727232a3e5f7a68f66c0832362b1b
Inderjeet Singh <meet2indersingh@gmail.com>
2020-08-30 15:51:37 +0530

    InitialCommit

---
+++ lib/models/products.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/foundation.dart';
+
+class Prdoucts {
+  final String id;
+  final String title;
+  final String description;
+  final double price;
+  final String imageUrl;
+  bool isFavorite;
+
+  Prdoucts({@required this.id, @required this.title, 
+  @required this.description, @required this.price, @required this.imageUrl, 
+  this.isFavorite = false });
+}
